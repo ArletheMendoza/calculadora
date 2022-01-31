f30238b4f9915c48b3df87a54bdd563bf6fb370e
ArletheMendoza <98105769+ArletheMendoza@users.noreply.github.com>
2022-01-31 11:07:55 -0600

    Add files via upload

---
+++ calculadora/calculadora.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    int p1, p2;
+    cout << "Ingrese un numero: ";
+    cin >> p1;
+    cout << "Ingrese otro numero: ";
+    cin >> p2;
+    cout << " La suma de los numeros es: " << p1 + p2 << endl ;
+    cout << " La resta de los numeros es: " << p1 - p2 << endl;
+    cout << " La multiplicación de los numeros es: " << p1 * p2 << endl;
+    cout << " La division de los numeros es: " << p1 / p2 << endl;
+    return 0;
+}
